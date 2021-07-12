491bad0036faf233c73a5ea9cab050a24d70b65c
Mattyx00 <matteosacco00@gmail.com>
2021-07-12 17:51:25 +0200

    + pulsante

--- src/main/webapp/functions/dashboard.js
+++ src/main/webapp/functions/dashboard.js
@@ -1,3 +1,20 @@
+function eliminaProdotto(id){
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            alert("Prodotto eliminato con successo!");
+            location.reload();
+        }
+    };
+    let url = "AdminServlet/eliminaProdotto?id="+id;
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+function modificaProdotto(id){
+
+}
+
 function loadTable(jsonProdotti){
     table = $('#tabella_prodotti').DataTable({
         "dom": '<"toolbar">frtip',
@@ -52,6 +69,22 @@ function loadTable(jsonProdotti){
     });
     $("#datagrid_visualizzaprodotti div.toolbar").html('<button onclick="aggiungiProdotto()" style="font-size: 15px; color: white; background-color: #0d6efd; border: none; cursor: pointer"><i style="padding: 10px" class="fas fa-plus"></i></button>');
 
+
+
+    $('#tabella_prodotti tbody').on('click', 'tr', function (e) {
+        if(e.target.getAttribute("id")=="modifica"||e.target.getAttribute("id")=="modifica_icon"){
+            if (e.target.nodeName == 'BUTTON' || e.target.nodeName == 'I') {
+                let id_prodotto = table.row( this ).data().id;
+            }
+        }else{
+            if(e.target.getAttribute("id")=="cancella"||e.target.getAttribute("id")=="elimina_icon"){
+                if (e.target.nodeName == 'BUTTON' || e.target.nodeName == 'I') {
+                    let id_prodotto = table.row( this ).data().id;
+                }
+            }
+        }
+
+    });
 }
 
 $( document ).ready(function() {
@@ -64,6 +97,4 @@ $( document ).ready(function() {
     let url = "AdminServlet/mostraProdotti";
     xhttp.open("GET", url, true);
     xhttp.send();
-
-
 });
